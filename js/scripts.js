56b8eabb1ea195705b5f045e11511eede3bcea01
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-18 10:18:47 -0700

    it will create javaScript with for loops
    Co-authored-by: Joseph Nilles <jbnilles24@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
-
+ $('#userForm').submit(function () {
+   event.preventDefault();
+  let sentence = $('#userInput').val().split('');
+  let printedSentence = sentence.slice();
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  for(let index = 0; index < sentence.length; index++){
+    for(let i = 0; i < vowels.length; i++)
+    {
+      if(vowels[i] === sentence[index].toLowerCase()) {
+        printedSentence[index] = '-';
+        break;
+      }
+    }
+  }
+   
+   $('#guess').text(printedSentence.join(''));
+ 
+ });
 
 });
+
